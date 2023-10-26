27704eee3d3fb51745ffab2496eea980ad5abe72
Miguel Tovar <99743970+Migtovarve@users.noreply.github.com>
2023-10-26 11:26:59 -0400

    -SRP

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import './style.css'
 // import './smells-code/01-singleton.js'
-import './smells-code/02-low-coupling'
+import './solid/01-srp'
 
 const app = document.querySelector<HTMLDivElement>('#app')!
 
